13a6d69eafee01cdd22d10ccb0e21bc92baadfa4
Scott Perry <dev@numist.net>
2017-02-20 20:07:43 -0800

    Initial Commit

---
+++ optargparse/optargparse.h
@@ -0,0 +1,13 @@
+//
+//  optargparse.h
+//  optargparse
+//
+//  Created by Scott Perry on 2/20/17.
+//  Copyright © 2017 Scott Perry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface optargparse : NSObject
+
+@end
---
+++ optargparse/optargparse.m
@@ -0,0 +1,13 @@
+//
+//  optargparse.m
+//  optargparse
+//
+//  Created by Scott Perry on 2/20/17.
+//  Copyright © 2017 Scott Perry. All rights reserved.
+//
+
+#import "optargparse.h"
+
+@implementation optargparse
+
+@end
